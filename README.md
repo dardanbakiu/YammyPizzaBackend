aa28388dc4bf77db2e18e501e5015e2d8fbb8567
Dardan Bakiu <dardan.bakiu@gmail.com>
2020-08-22 20:37:53 +0200

    Order Pizza back end

---
+++ README.md
@@ -0,0 +1,14 @@
+
+## About This App
+
+Laravel is a web application framework with expressive, elegant syntax. We believe development must be an enjoyable and creative experience to be truly fulfilling. Laravel takes the pain out of development by easing common tasks used in many web projects, such as:
+
+Yammy Pizza is an app to order pizza online, its simple to use, you only have to 
+select products, write your full name, location and then click buy button
+
+## App install 
+
+Pull the project and then open a bash or powershell on back-end write this command : "php artisan serve"
+go to the frontend folder, open another shell here and write "npm start" 
+
+Website will run at localhost:3000 
---
+++ app/Http/Controllers/OrderApi.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Order;
+class OrderApi extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        $result = Order::all();
+        return $result;
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+        $order = new Order();
+        $order->pizzaType = $request->products;
+        $order->date = $request->date; // working
+        $order->fullName = $request->fullname;// working
+        $order->location = $request->location;// working 
+        $order->price = $request->totalPrice; // working
+
+        $order->save();
+
+        return (['status'=>'success','msg'=>"Yammy, your will get the order in one hour, see your order at Orders field on Navigation Bar"]);
+  
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
